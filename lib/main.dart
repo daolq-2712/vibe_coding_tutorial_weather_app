312b5750d2bac30525b906aecd30c72419368601
le.quang.dao <le.quang.dao@sun-asterisk.com>
2025-03-03 22:59:39 +0700

    Finsih presentation layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'core/weather_page.dart';
+import 'weather/presentation/weather_page.dart';
 
 void main() {
   runApp(const WeatherApp());
--- lib/weather/presentation/weather_page.dart
+++ lib/weather/presentation/weather_page.dart
@@ -68,4 +68,4 @@ class WeatherPage extends StatelessWidget {
       ),
     );
   }
-} 
+}
